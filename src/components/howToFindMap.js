27493e3799afc1171ab4eb03f6aaff22ce58e7b1
Mikhail <d14bl0@mail.ru>
2022-09-23 16:43:20 +0300

    fix support-us and how-to-find

---
+++ src/components/howToFindMap.js
@@ -0,0 +1,24 @@
+var myMap;
+
+ymaps.ready(init);
+
+function init () {
+    myMap = new ymaps.Map('map', {
+        center: [44.321946, 41.919865],
+        zoom: 15,
+        controls: ['zoomControl']
+    }, {
+        searchControlProvider: 'yandex#search'
+    });
+
+    myMap.behaviors
+        .disable(['rightMouseButtonMagnifier'])
+    myMap.options.set('scrollZoomSpeed', 3);
+var myPlacemark = new ymaps.Placemark(
+			// Координаты метки
+			[44.321946, 41.919865]
+			);
+
+		// Добавление метки на карту
+		myMap.geoObjects.add(myPlacemark);
+}
